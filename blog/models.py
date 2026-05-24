4b77f25bfac3f5cdffe1a0edec1b7393465b342d
Nikhil-2717 <nikhilv97545@gmail.com>
2026-05-24 17:32:57 +0530

    blog app

---
+++ blog/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+class Category(models.Model):
+    category_name=models.CharField(max_length=50,unique=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.category_name
+
+ # Here we are mkaing this beacuse we want the status of teh blog to be either draft or published so we will use it as dropdown   
+STATUS_BLOG =( 
+    ("draft","Draft"),
+    ("published","Published")
+)
+    
+class Blog(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150,unique=True,blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author=models.ForeignKey(User , on_delete=models.CASCADE)
+    featured_image = models.ImageField(upload_to='uplaod/%Y/%m/%d') #we are creating a folder and mentionig the date time year for the image 
+    
+    short_description = models.TextField(max_length=500)
+    blog_body =models.TextField(max_length=2000)
+    status = models.CharField(max_length=20,choices=STATUS_BLOG)
+    featured = models.BooleanField(default=False)
+    created_at= models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
