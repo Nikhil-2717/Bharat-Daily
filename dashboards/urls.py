890b1bc7640b0c4515194ccf595a107479cb9e01
Nikhil-2717 <nikhilv97545@gmail.com>
2026-05-27 20:15:43 +0530

    recent Commit

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -14,6 +14,12 @@ urlpatterns=[
     path('posts/',views.posts,name='posts'),
     path('posts/add_post',views.add_post,name='add_post'),
     path('posts/edit_post/<int:pk>',views.edit_post,name='edit_post'),
-    path('posts/delete_post/<int:pk>',views.delete_post,name='delete_post')
+    path('posts/delete_post/<int:pk>',views.delete_post,name='delete_post'),
+
+    #Users
+    path('users/',views.users , name='users'),
+    path('users/add', views.add_user,name='add_user'),
+    path('users/edit_user/<int:pk>',views.edit_user,name='edit_user'),
+    path('users/delete_user/<int:pk>',views.delete_user,name='delete_user')
     
 ]
